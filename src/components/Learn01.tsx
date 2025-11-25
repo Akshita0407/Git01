ccdf71db30a4321585005b499e44a15f61a474fd
Akshita <akshitaa0304@gmail.com>
2025-11-25 14:35:16 +0530

    Initial Commit

---
+++ src/components/Learn01.tsx
@@ -0,0 +1,14 @@
+
+
+const Learn01 = () => {
+  return (
+    <div>
+      <form >
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  )
+}
+
+export default Learn01
